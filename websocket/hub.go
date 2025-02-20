0677588882d0928a475dadee84fce520118282cc
Davi0805 <melocardozo.video@gmail.com>
2025-02-20 12:10:48 +0000

    feature(websocket): Hub de conexoes ws

---
+++ websocket/hub.go
@@ -0,0 +1,65 @@
+package websocket
+
+import (
+    "github.com/Davi0805/gnose-notification/models"
+    "github.com/Davi0805/gnose-notification/service"
+    "github.com/gofiber/websocket/v2"
+)
+
+type Client struct {
+    Conn *websocket.Conn
+}
+
+type Hub struct {
+    clients    map[*Client]bool
+    broadcast  chan models.Message
+    register   chan *Client
+    unregister chan *Client
+    service    *service.MessageService
+}
+
+func NewHub(service *service.MessageService) *Hub {
+    return &Hub{
+        clients:    make(map[*Client]bool),
+        broadcast:  make(chan models.Message),
+        register:   make(chan *Client),
+        unregister: make(chan *Client),
+        service:    service,
+    }
+}
+
+func (h *Hub) Run() {
+    for {
+        select {
+        case client := <-h.register:
+            h.clients[client] = true
+        case client := <-h.unregister:
+            if _, ok := h.clients[client]; ok {
+                delete(h.clients, client)
+                client.Conn.Close()
+            }
+        case message := <-h.broadcast:
+            h.service.SaveMessage(message)
+            for client := range h.clients {
+                err := client.Conn.WriteJSON(message)
+                if err != nil {
+                    client.Conn.Close()
+                    delete(h.clients, client)
+                }
+            }
+        }
+    }
+}
+
+func (h *Hub) Register(client *Client) {
+    h.register <- client
+}
+
+func (h *Hub) Unregister(client *Client) {
+    h.unregister <- client
+}
+
+// BROADCAST PARA TODOS OS CLIENTS
+func (h *Hub) Broadcast(message models.Message) {
+    h.broadcast <- message
+}
