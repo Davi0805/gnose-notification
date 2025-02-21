9462b241cfbc16660fd950b84e52c27a9896f94c
Davi0805 <melocardozo.video@gmail.com>
2025-02-21 20:42:22 +0000

    feat(controller):  http message Controller get e getcompanyid com auth

--- controllers/message_controller.go
+++ controllers/message_controller.go
@@ -4,39 +4,104 @@ import (
     "github.com/gofiber/fiber/v2"
     "github.com/Davi0805/gnose-notification/service"
 	"strconv"
+	"context"
 )
 
 type MessageController struct {
     service *service.MessageService
+	authService *service.AuthService
 }
 
-func NewMessageController(service *service.MessageService) *MessageController {
-    return &MessageController{service: service}
+func NewMessageController(service *service.MessageService, authService *service.AuthService) *MessageController {
+    return &MessageController{service: service, authService: authService}
 }
 
 func (c *MessageController) GetMessages(ctx *fiber.Ctx) error {
-    messages, err := c.service.GetAllMessages()
+
+	// GET HEADER
+    authHeader := ctx.Get("Authorization")
+    if authHeader == "" {
+        return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+            "ERRO": "Authorization header nao encontrado",
+        })
+    }
+
+    // GET CREDENTIALS
+    credentials, err := c.authService.GetCredentialsFromToken(context.Background(), authHeader)
+    if err != nil {
+        return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+            "ERRO": err.Error(),
+        })
+    }
+
+	// GET EMPRESAS DO USUARIO
+    companyIds := c.getCompanyIds(credentials)
+
+
+    messages, err := c.service.GetAllMessages(companyIds)
     if err != nil {
         return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-            "error": "Failed to fetch messages",
+            "ERRO": "Mensagem nao encontrada",
         })
     }
     return ctx.JSON(messages)
 }
 
 func (c *MessageController) GetMessagesByCompanyId(ctx *fiber.Ctx) error {
+
+
     companyIdStr := ctx.Params("companyId")
     companyId, err := strconv.ParseInt(companyIdStr, 10, 64) // BASE 10 e 64 bits
     if err != nil {
         return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-            "error": "Invalid company ID",
+            "ERRO": "Company id invalido",
         })
     }
+
+	// GET HEADER
+    authHeader := ctx.Get("Authorization")
+    if authHeader == "" {
+        return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+            "ERRO": "Authorization header nao encontrado",
+        })
+    }
+
+    // GET CREDENTIALS
+    credentials, err := c.authService.GetCredentialsFromToken(context.Background(), authHeader)
+    if err != nil {
+        return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+            "ERRO": err.Error(),
+        })
+    }
+
+    // CHECA SE FAZ PARTE DA EMPRESA
+    isAuthorized := false
+    for _, cred := range credentials {
+        if cred.ID.CompanyId == int(companyId) {
+            isAuthorized = true
+            break
+        }
+    }
+
+    if !isAuthorized {
+        return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+            "ERRO": "User nao faz parte da empresa",
+        })
+    }
+
     messages, err := c.service.GetMessagesByCompanyId(companyId)
     if err != nil {
         return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-            "error": "Failed to fetch messages",
+            "ERRO": "Mensagem nao encontrada",
         })
     }
     return ctx.JSON(messages)
+}
+
+func (c *MessageController) getCompanyIds(credentials []service.TokenData) []int64 {
+    var companyIds []int64
+    for _, cred := range credentials {
+        companyIds = append(companyIds, int64(cred.ID.CompanyId))
+    }
+    return companyIds
 }
