92423dcf596376df4751650e05ba14d07716a247
Davi0805 <melocardozo.video@gmail.com>
2025-02-21 16:30:20 +0000

    feat(auth): credentials parsed and stored locally

--- middleware/auth.go
+++ middleware/auth.go
@@ -3,11 +3,20 @@ package middleware
 import (
     "context"
     "github.com/gofiber/fiber/v2"
+    "encoding/json"
     /* "github.com/golang-jwt/jwt/v4" */
     "github.com/redis/go-redis/v9"
 	/* "log" */
-    /* "fmt" */
+    "fmt"
 )
+
+type TokenData struct {
+    ID struct {
+        UserId    int `json:"userId"`
+        CompanyId int `json:"companyId"`
+    } `json:"id"`
+    Permission string `json:"permission"`
+}
 
 func JWTMiddleware(redisClient *redis.Client) fiber.Handler {
     return func(c *fiber.Ctx) error {
@@ -39,6 +48,39 @@ func JWTMiddleware(redisClient *redis.Client) fiber.Handler {
         if err != nil || exists == 0 {
             return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"Erro": "Token invalido"})
         }
+
+        // TODO: REFATORAR E LIMPAR PARA APENAS 1 QUERY QUANDO JA ESTIVER ESTAVEL
+        // GET CREDENCIAIS DO REDIS
+        jsonData, err := redisClient.Get(ctx, tokenString).Result()
+        if err != nil {
+            return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"Erro": "Erro ao buscar dados do token"})
+        }
+
+        // PARSE
+        var tokenData []TokenData
+        err = json.Unmarshal([]byte(jsonData), &tokenData)
+        if err != nil {
+            return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"Erro": "Erro ao parsear dados do token"})
+        }
+
+        // CHECA SE EXISTE CREDENCIAL
+        if len(tokenData) == 0 {
+            return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"Erro": "Dados do token nao encontrados"})
+        }
+
+        userId := tokenData[0].ID.UserId
+        var companyIds []int
+        for _, data := range tokenData {
+            companyIds = append(companyIds, data.ID.CompanyId)
+        }
+
+        // DEBUG
+        fmt.Println("Fetched userId:", userId)
+        fmt.Println("Fetched companyId:", companyIds)
+
+        // ENV VARIABLES
+        c.Locals("userId", userId)
+        c.Locals("companyId", companyIds)
 
         return c.Next()
     }
